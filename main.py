db4c406d122fdd5000ebd26febd381383573127a
あずねーう <prsk.iroha0513@icloud.com>
2025-06-18 07:50:17 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -221,6 +221,9 @@ async def on_ready():
             
             await bot.load_extension("commands.general.pjsk_list_song_commands")
             logger.info("デバッグ: commands.general.pjsk_list_song_commands がロードされました。")
+            
+            await bot.load_extension("commands.general.pjsk_rankmatch_song_commands")
+            logger.info("デバッグ: commands.general.pjsk_rankmatch_song_commands がロードされました。")
 
         except Exception as e:
             logger.error(f"エラー: コグのロード中にエラーが発生しました: {e}", exc_info=True)
